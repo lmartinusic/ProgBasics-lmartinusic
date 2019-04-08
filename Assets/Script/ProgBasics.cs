0ee48c3301d1d9b1224c8baa08d54d604c6d93be
Leo Martinusic <lmartinusic@leos-mbp.home>
2019-04-08 22:33:41 +0200

    Multiplikative Operatoren

--- Assets/Script/ProgBasics.cs
+++ Assets/Script/ProgBasics.cs
@@ -6,6 +6,10 @@ public class ProgBasics : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Multiplikative Operatoren
+		Debug.Log("Multiplikative Operatoren: * " + (3*4));
+		Debug.Log("Multiplikative Operatoren: / " + (12/4));
+		Debug.Log("Multiplikative Operatoren: % " + (3%4));
 		
 	}
 	
