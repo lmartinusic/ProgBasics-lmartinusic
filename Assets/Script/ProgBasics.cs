e6328fcd49d35e31f2a736af2c43988b0d6a5acc
Leo Martinusic <lmartinusic@leos-mbp.home>
2019-04-08 22:40:26 +0200

    Additive Operatoren

--- Assets/Script/ProgBasics.cs
+++ Assets/Script/ProgBasics.cs
@@ -6,11 +6,16 @@ public class ProgBasics : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 		// Multiplikative Operatoren
 		Debug.Log("Multiplikative Operatoren: * " + (3*4));
 		Debug.Log("Multiplikative Operatoren: / " + (12/4));
 		Debug.Log("Multiplikative Operatoren: % " + (3%4));
 		
+		//Additive Operatoren
+		Debug.Log("Additive Operatoren: + " + (3+4));
+		Debug.Log("Additive Operatoren: - " + (8-4));
+
 	}
 	
 	// Update is called once per frame
